7b25b861170e0bc0f10f108d35cc71d0af5f083e
MichaelRoeder <michaderstudent@hotmail.de>
2014-06-26 20:03:24 +0200

    Added a test for the matrix creation.

--- src/test/java/org/aksw/sandbox/AxelsPreprocessingMatrixCreationTest.java
+++ src/test/java/org/aksw/sandbox/AxelsPreprocessingMatrixCreationTest.java
@@ -5,72 +5,56 @@ import java.util.Collection;
 
 import junit.framework.Assert;
 
-import org.aksw.sandbox.datatypes.DocumentWithPositions;
 import org.aksw.sandbox.datatypes.Entity;
 import org.aksw.sandbox.datatypes.TokenizedDocument;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.la4j.matrix.Matrix;
+import org.la4j.matrix.dense.Basic2DMatrix;
 
 @RunWith(Parameterized.class)
 public class AxelsPreprocessingMatrixCreationTest {
 
     @Parameters
     public static Collection<Object[]> data() {
-        return Arrays
-                .asList(new Object[][] {
-                        { "Dieser Text ist ein Testtext.", new String[] { "Dieser", "Text", "ist", "ein", "Testtext" },
-                                new Entity[] {},
-                                new int[][] {} },
-                        { "Der neue Jaguar ist kein Golf.",
-                                new String[] { "Der", "neue", "Jaguar", "ist", "kein", "Golf" },
-                                new Entity[] { new Entity(9, 15, "http://car/Jaguar", "Jaguar"),
-                                        new Entity(25, 29, "http://car/Golf", "Golf") },
-                                new int[][] { { 2, 3 }, { 5, 6 } } },
-                        { "Am persischen Golf wird wieder Golf gespielt!",
-                                new String[] { "Am", "persischen", "Golf", "wird", "wieder", "Golf", "gespielt" },
-                                new Entity[] { new Entity(3, 18, "http://geo/persianGolf", "persischen Golf"),
-                                        new Entity(31, 35, "http://car/Golf", "Golf") },
-                                new int[][] { { 1, 3 }, { 5, 6 } } },
-                        // Let's check all 3 sentences together
-                        {
-                                "Dieser Text ist ein Testtext. Der neue Jaguar ist kein Golf. Am persischen Golf wird wieder Golf gespielt! Entity",
-                                new String[] { "Dieser", "Text", "ist", "ein", "Testtext", "Der", "neue", "Jaguar",
-                                        "ist", "kein", "Golf", "Am", "persischen", "Golf", "wird", "wieder", "Golf",
-                                        "gespielt", "Entity" },
-                                new Entity[] { new Entity(39, 45, "http://car/Jaguar", "Jaguar"),
-                                        new Entity(55, 59, "http://car/Golf", "Golf"),
-                                        new Entity(64, 79, "http://geo/persianGolf", "persischen Golf"),
-                                        new Entity(92, 96, "http://car/Golf", "Golf"),
-                                        new Entity(107, 113, "http://entity", "Entity") },
-                                new int[][] { { 7, 8 }, { 10, 11 }, { 12, 14 }, { 16, 17 }, { 18, 19 } }
-
-                        } });
+        return Arrays.asList(new Object[][] { {
+                new TokenizedDocument[] {
+                        /*
+                         * tokenIds = "Der"=0, "neue"=1, "Jaguar"=2, "ist"=3,
+                         * "kein"=4, "Golf"=5, "Am"=6, "persischen"=7, "wird"=8,
+                         * "wieder"=9, "gefahren"=10
+                         * 
+                         * entityIds = Jaguar=0, Golf=1, persianGolf=2
+                         */
+                        new TokenizedDocument(new String[] { "Der", "neue", "Jaguar", "ist", "kein", "Golf" },
+                                new Entity[] { new Entity(2, 3, "http://car/Jaguar", "Jaguar"),
+                                        new Entity(5, 6, "http://car/Golf", "Golf") }),
+                        new TokenizedDocument(new String[] { "Am", "persischen", "Golf", "wird", "wieder", "Golf",
+                                "gefahren" }, new Entity[] {
+                                new Entity(1, 3, "http://geo/persianGolf", "persischen Golf"),
+                                new Entity(5, 6, "http://car/Golf", "Golf") }) },
+                new double[][] { { 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1 },
+                        { 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0 } } } });
     }
 
-    private String text;
-    private String tokens[];
-    private Entity entities[];
-    private int expectedNewPositions[][];
+    private TokenizedDocument documents[];
+    private double expectedMatrix[][];
 
-    public AxelsPreprocessingMatrixCreationTest(String text, String[] tokens, Entity[] entities,
-            int[][] expectedNewPositions) {
-        this.text = text;
-        this.tokens = tokens;
-        this.entities = entities;
-        this.expectedNewPositions = expectedNewPositions;
+    public AxelsPreprocessingMatrixCreationTest(TokenizedDocument documents[], double expectedMatrix[][]) {
+        this.documents = documents;
+        this.expectedMatrix = expectedMatrix;
     }
 
     @Test
     public void test() {
-        TokenizedDocument documents[] = AxelsPreprocessing
-                .mapEntitiesToTokens(new DocumentWithPositions[] { new DocumentWithPositions(text, tokens,
-                        entities) });
-        Assert.assertEquals(expectedNewPositions.length, documents[0].entities.length);
-        for (int i = 0; i < documents[0].entities.length; ++i) {
-            Assert.assertEquals(expectedNewPositions[i][0], documents[0].entities[i].start);
-            Assert.assertEquals(expectedNewPositions[i][1], documents[0].entities[i].end);
+        Matrix matrix = AxelsPreprocessing.createMatrix(documents, 3);
+        for (int i = 0; i < expectedMatrix.length; i++) {
+            for (int k = 0; k < expectedMatrix[i].length; k++) {
+                Assert.assertEquals(matrix.toString() + " does not equal the expected "
+                        + (new Basic2DMatrix(expectedMatrix)).toString(), expectedMatrix[i][k], matrix.get(i, k));
+            }
         }
     }
 }
